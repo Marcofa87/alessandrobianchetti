16c4f0d5c39c5b959d95cde03c45fb195e7b4599
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-10-22 22:45:26 +0200

    implemented logic for displaying language flag

--- app/ui/navbar/language-flags/LanguageFlags.tsx
+++ app/ui/navbar/language-flags/LanguageFlags.tsx
@@ -1,24 +1,27 @@
 import Image from "next/image";
 import englishFlag from "@/components/public/united-kingdom.png";
 import italianFlag from "@/components/public/italy.png";
+import useToggle from "@/components/hooks/useToggle";
 export default function LanguageFlags() {
+  const { isOpen, toggleMenu } = useToggle();
   return (
     <div className="absolute left-1/2 transform -translate-x-1/2 flex space-x-4">
-      <button>
-        <Image
-          src={italianFlag}
-          alt="italian flag language"
-          width={32}
-          height={32}
-        />
-      </button>
-      <button>
-        <Image
-          src={englishFlag}
-          alt="english flag language"
-          width={32}
-          height={32}
-        />
+      <button onClick={toggleMenu}>
+        {isOpen ? (
+          <Image
+            src={italianFlag}
+            alt="italian flag language"
+            width={24}
+            height={24}
+          />
+        ) : (
+          <Image
+            src={englishFlag}
+            alt="english flag language"
+            width={24}
+            height={24}
+          />
+        )}
       </button>
     </div>
   );
