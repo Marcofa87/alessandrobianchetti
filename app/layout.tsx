57750efc46d332bca9930b5359c03a503c301b53
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-10-22 12:47:48 +0200

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { roboto } from "./ui/fonts";
+import "./styles/globals.css";
+import Navbar from "./ui/navbar/Navbar";
+
+export const metadata: Metadata = {
+  title: "Alessandro Bianchetti",
+  description: "Alessandro Bianchetti Personal Trainer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.className} antialiased`}>
+        <nav className="border-slate-600">
+          <Navbar />
+        </nav>
+        <main>{children} </main>
+      </body>
+    </html>
+  );
+}
---
+++ app/ui/navbar/Navbar.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Menu, X } from "lucide-react";
+import logo from "@/components/public/danielle-cerullo-CQfNt66ttZM-unsplash.jpg";
+import englishFlag from "@/components/public/united-kingdom.png";
+import italianFlag from "@/components/public/italy.png";
+
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
+  return (
+    <nav className="p-4 relative">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="logo">
+          <Image
+            src={logo}
+            alt="Personal Trainer Logo"
+            width={100}
+            height={100}
+            className="rounded-full"
+          />
+        </div>
+
+        {/* Centered flags for both mobile and desktop */}
+        <div className="absolute left-1/2 transform -translate-x-1/2 flex space-x-4">
+          <Image
+            src={italianFlag}
+            alt="italian flag language"
+            width={32}
+            height={32}
+          />
+          <Image
+            src={englishFlag}
+            alt="english flag language"
+            width={32}
+            height={32}
+          />
+        </div>
+
+        {/* Toggle button for mobile menu */}
+        <div className="md:hidden">
+          <button
+            onClick={toggleMenu}
+            className="focus:outline-none"
+            aria-label={isOpen ? "Close menu" : "Open menu"}
+          >
+            {isOpen ? <X size={32} /> : <Menu size={32} />}
+          </button>
+        </div>
+
+        {/* Menu items for larger screens */}
+        <div className="hidden md:flex items-center space-x-4">
+          <Link
+            href="/structure"
+            className="hover:text-accent transition-colors duration-200"
+          >
+            Struttura
+          </Link>
+          <Link
+            href="/contacts"
+            className="hover:text-accent transition-colors duration-200"
+          >
+            Contatti
+          </Link>
+        </div>
+      </div>
+
+      {/* Mobile menu - Dropdown */}
+      <div
+        className={`absolute right-0 p-4 shadow-md transition-all duration-300 ease-in-out ${
+          isOpen ? "top-full opacity-100" : "top-[-200%] opacity-0"
+        } md:hidden`}
+      >
+        <ul className="space-y-4">
+          <li>
+            <Link
+              href="/structure"
+              className="block hover:text-accent transition-colors duration-200"
+              onClick={toggleMenu}
+            >
+              Struttura
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/contacts"
+              className="block hover:text-accent transition-colors duration-200"
+              onClick={toggleMenu}
+            >
+              Contatti
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
