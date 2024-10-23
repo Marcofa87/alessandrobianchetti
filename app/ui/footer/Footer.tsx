de7cea40e2e906ad13644b3a677e69d8fdc1729a
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-10-23 12:30:55 +0200

    implemented style

---
+++ app/ui/footer/Footer.tsx
@@ -0,0 +1,10 @@
+import { FiInstagram, FiLinkedin } from "react-icons/fi";
+
+export default function Footer() {
+  return (
+    <div>
+      <FiInstagram />
+      <FiLinkedin />
+    </div>
+  );
+}
