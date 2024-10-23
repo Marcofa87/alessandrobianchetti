98c20f64a70c31db01eb1d038015cd235cc7d26a
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-10-23 11:56:12 +0200

    implmented map location

--- app/ui/home/HomePage.tsx
+++ app/ui/home/HomePage.tsx
@@ -3,10 +3,11 @@ import Image from "next/image";
 
 import gymMainImage from "@/components/public/victor-freitas-WvDYdXDzkhs-unsplash.jpg";
 import Accordion from "./accordion/Accordion";
+import MapLocation from "./locationmap/MapLocation";
 
 export default function HomePage() {
   return (
-    <div className="w-3/4 md:max-w-3xl mx-auto space-y-2">
+    <div className="w-3/4 md:max-w-3xl mx-auto space-y-12">
       <Image
         src={gymMainImage}
         alt="immagine della palestra"
@@ -14,6 +15,7 @@ export default function HomePage() {
         className="m-auto w-2/3 mb-12"
       />
       <Accordion />
+      <MapLocation />
     </div>
   );
 }
