f1ffb01332ed5f0d199ca965dd744e6b76b373ec
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-10-25 17:51:22 +0200

    test email issue

--- lib/actions.ts
+++ lib/actions.ts
@@ -11,7 +11,8 @@ export async function sendEmail(data: {
 }) {
   try {
     const transporter = nodemailer.createTransport({
-      service: "gmail",
+      host: process.env.SMTP_HOST,
+      port: parseInt(process.env.SMTP_PORT || "587"),
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
