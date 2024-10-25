31f303e1cfe06d850d5c9ea49bea85628da03dde
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-10-25 18:13:13 +0200

    modifiyng actions.ts

--- lib/actions.ts
+++ lib/actions.ts
@@ -12,22 +12,24 @@ export async function sendEmail(data: {
   try {
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
-      port: parseInt(process.env.SMTP_PORT || "587"),
-      secure: false,
+      port: Number(process.env.SMTP_PORT), // Assicurati che la porta sia un numero
+      secure: process.env.SMTP_PORT === "465", // Usa `secure` solo con la porta 465
       auth: {
         user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
+        pass: process.env.SMTP_PASS, // Password per app generata per l'autenticazione a 2 fattori
       },
+      debug: true, // Abilita debug
+      logger: true, // Abilita il log per vedere i dettagli delle operazioni
     });
 
     const mailOptions = {
       from: process.env.SMTP_USER,
       to: "marco.falasca87@gmail.com",
-      replyTo: data.email, // This allows you to reply directly to the sender
+      replyTo: data.email, // Permette di rispondere direttamente al mittente
       subject: `New contact form submission: ${data.subject}`,
       text: `
         Name: ${data.name}
-        Number: ${data.phone}
+        Phone: ${data.phone}
         Email: ${data.email}
         Subject: ${data.subject}
         Message: ${data.message}
