a4d64d0e2e7dbf8f4cdf5c9b19aed96998537140
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-10-26 15:40:33 +0200

    implemented dumbell icon when form is submitted

--- app/ui/success/SuccessPage.tsx
+++ app/ui/success/SuccessPage.tsx
@@ -1,4 +1,4 @@
-import { Dumbbell } from "lucide-react";
+import { BicepsFlexed } from "lucide-react";
 import Link from "next/link";
 
 export default function SuccessSubmit() {
@@ -6,9 +6,9 @@ export default function SuccessSubmit() {
     <div className="flex flex-col items-center mt-52 ">
       <h2 className=" mb-4">Successo!</h2>
       <p className="text-lg  mb-6">Il tuo form è stato inviato con successo.</p>
-      <Dumbbell size={128} />
+      <BicepsFlexed className="my-8" size={128} color="#c1292e" />
       <Link href="/" passHref>
-        <button className="px-4 py-2 bg-[var(--tertiary-color)]  rounded hover:bg-red-800 transition">
+        <button className="px-4 py-2 bg-[var(--tertiary-color)]  rounded hover:bg-red-800 transition font-bold">
           Torna alla Home
         </button>
       </Link>
