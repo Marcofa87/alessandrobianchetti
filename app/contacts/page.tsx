9f1a8cb65199fcb79d29e1645e980cecaa291f2c
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-10-27 21:01:21 +0100

    row-reversed contacts page components when size is md:

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,10 +1,10 @@
-import ContactForm from "../../ui/contacts/contact-form";
-import ContactPage from "../../ui/contacts/contact-page";
+import ContactForm from "@/components/ui/contacts/contact-form";
+import ContactPage from "@/components/ui/contacts/contact-page";
 
 export default function Contacts() {
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="flex flex-col items-center md:flex-row  md:justify-between space-y-8 md:space-y-0 md:space-x-8">
+      <div className="flex flex-col items-center md:flex-row-reverse  md:justify-between space-y-8 md:space-y-0 md:space-x-8">
         <div className="w-full max-w-md">
           <ContactPage />
         </div>
--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,4 +1,4 @@
-import SuccessSubmit from "../ui/success/SuccessPage";
+import SuccessSubmit from "@/components/ui/success/SuccessPage";
 
 export default function SuccessPage() {
   return <SuccessSubmit />;
