9ea7ddf577f0c001309421ffb4f7831fd1a7e44e
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-10-28 12:46:46 +0100

    changhed layout for desktop version for contact page

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -4,7 +4,7 @@ import ContactPage from "@/components/ui/contacts/contact-page";
 export default function Contacts() {
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="flex flex-col items-center md:flex-row-reverse  md:justify-between space-y-8 md:space-y-0 md:space-x-8">
+      <div className="flex flex-col items-center lg:flex-row-reverse  lg:justify-between space-y-8 md:space-y-0 md:space-x-8">
         <div className="w-full max-w-md">
           <ContactPage />
         </div>
