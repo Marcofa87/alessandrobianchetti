cac9d70073be1eeed30888321ee8c60a0c4bb59a
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-10-30 19:03:10 +0100

    changed iframe map location

--- app/ui/home/locationmap/MapLocation.tsx
+++ app/ui/home/locationmap/MapLocation.tsx
@@ -5,7 +5,7 @@ export default function MapLocation() {
       <div className="relative w-full h-0 pb-[56.25%] m-auto sm:pb-[75%]">
         {/* Proporzioni 16:9 su desktop, 4:3 su mobile */}
         <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d762.1204577249193!2d12.521261629064592!3d41.89135588033563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132f618ee46ec001%3A0x7e1beabc34025ecd!2sVia%20Prenestina%2C%2060%2C%2000176%20Roma%20RM!5e0!3m2!1sit!2sit!4v1729674890696!5m2!1sit!2sit"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2970.0823483175045!2d12.519581476484426!3d41.89108606460039!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132f61024cde5375%3A0x1a57d377453a50e1!2skamehouse%20studio!5e0!3m2!1sit!2sit!4v1730311292153!5m2!1sit!2sit"
           className="absolute top-0 left-0 w-full h-full border-2 border-[var(--tertiary-color)]"
           allowFullScreen={true}
           loading="lazy"
