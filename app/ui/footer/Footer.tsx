7ea66743bc3da78dbbde9a6ff828a7d44aa3000c
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-10-30 19:29:36 +0100

    wrapped social icons into links

--- app/ui/footer/Footer.tsx
+++ app/ui/footer/Footer.tsx
@@ -4,10 +4,21 @@ import { FiFacebook, FiInstagram, FiLinkedin } from "react-icons/fi";
 export default function Footer() {
   return (
     <footer className="flex justify-end w-3/4 h-20 md:max-w-3xl mx-auto mt-12">
-      <FiInstagram className="mr-4 text-2xl" />
-      <FiLinkedin className="text-2xl " />
-      <FaWhatsapp className="text-2xl" />
-      <FiFacebook className="text-2xl" />
+      <a href="https://www.instagram.com/alexroma84/" target="_blanl">
+        <FiInstagram className="mr-4 text-2xl" />
+      </a>
+      <a href="https://www.linkedin.com/in/alessandro-bianchetti-53562a88/">
+        <FiLinkedin className="text-2xl " />
+      </a>
+      <a
+        href="https://www.facebook.com/alessandro.bianchetti.39/"
+        target="_blank"
+      >
+        <FiFacebook className="text-2xl" />
+      </a>
+      <a href="">
+        <FaWhatsapp className="text-2xl" />
+      </a>
     </footer>
   );
 }
