b442d00aaffc6c827a137d9e130c15109374fb15
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-10-30 19:13:49 +0100

    implemented social icons

--- app/ui/footer/Footer.tsx
+++ app/ui/footer/Footer.tsx
@@ -1,10 +1,13 @@
-import { FiInstagram, FiLinkedin } from "react-icons/fi";
+import { FaWhatsapp } from "react-icons/fa";
+import { FiFacebook, FiInstagram, FiLinkedin } from "react-icons/fi";
 
 export default function Footer() {
   return (
     <footer className="flex justify-end w-3/4 h-20 md:max-w-3xl mx-auto mt-12">
       <FiInstagram className="mr-4 text-2xl" />
       <FiLinkedin className="text-2xl " />
+      <FaWhatsapp className="text-2xl" />
+      <FiFacebook className="text-2xl" />
     </footer>
   );
 }
