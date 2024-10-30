1ca2f03fcc0468994c2e5d4dbc76e33b1e78c266
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-10-30 18:51:16 +0100

    test

--- lib/actions.ts
+++ lib/actions.ts
@@ -10,13 +10,6 @@ export async function sendEmail(data: {
   message: string;
 }) {
   try {
-    console.log({
-      SMTP_HOST: process.env.SMTP_HOST,
-      SMTP_PORT: process.env.SMTP_PORT,
-      SMTP_USER: process.env.SMTP_USER,
-      SMTP_PASS: process.env.SMTP_PASS ? "********" : "not set",
-    });
-
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
       port: Number(process.env.SMTP_PORT), // Assicurati che la porta sia un numero
