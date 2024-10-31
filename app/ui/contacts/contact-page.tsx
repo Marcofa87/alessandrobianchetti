24f3d0ea673e6efac81c3fab4881fd70cd92f1b7
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-10-31 19:46:51 +0100

    implemented transitions to contact-page when hovering incons

--- app/ui/contacts/contact-page.tsx
+++ app/ui/contacts/contact-page.tsx
@@ -1,58 +1,54 @@
-import { MdLocationOn } from "react-icons/md";
-import { FaMobileAlt } from "react-icons/fa";
-import { MdAlternateEmail } from "react-icons/md";
+import { Phone, Mail, MapPin } from "lucide-react";
+
 export default function ContactPage() {
   return (
     <div className="max-w-md mx-auto mt-10 p-6 sm:my-16 bg-stone-100 rounded-xl shadow-md">
-      <h2 className=" mb-6 text-[var(--color)]">Informazioni</h2>
+      <h2 className="mb-6 text-[var(--color)]">Informazioni</h2>
       <div className="space-y-4">
         <div className="flex items-center">
           <div className="flex-shrink-0 w-10 h-10 bg-indigo-100 rounded-full flex items-center justify-center">
-            {/* Phone icon placeholder */}
-            <FaMobileAlt className=" text-[var(--color)] text-2xl" />
+            <Phone className="text-[var(--color)] w-6 h-6" />
           </div>
           <div className="ml-4">
-            <p className=" font-medium text-[var(--color)]">
+            <p className="font-medium text-[var(--color)]">
               Numero di telefono
             </p>
-
-            <p className=" text-[var(--color)] underline hover:font-bold">
-              {" "}
-              <a href="https://wa.me/393394955810" target="_blank">
-                +39 3394955810
-              </a>
-            </p>
+            <a
+              href="https://wa.me/393394955810"
+              target="_blank"
+              className="text-[var(--color)] underline transition-all duration-500 hover:font-bold"
+            >
+              +39 3394955810
+            </a>
           </div>
         </div>
         <div className="flex items-center">
           <div className="flex-shrink-0 w-10 h-10 bg-indigo-100 rounded-full flex items-center justify-center">
-            {/* Email icon placeholder */}
-            <MdAlternateEmail className=" text-[var(--color)] text-2xl" />
+            <Mail className="text-[var(--color)] w-6 h-6" />
           </div>
           <div className="ml-4">
-            <p className=" font-medium text-[var(--color)] ">Indirizzo email</p>
-            <p className=" text-[var(--color)] hover:font-bold underline">
-              <a href="mailto:alessandrobianchetti.1984@gmail.com">
-                alessandrobianchetti.1984@gmail.com
-              </a>
-            </p>
+            <p className="font-medium text-[var(--color)]">Indirizzo email</p>
+            <a
+              href="mailto:alessandrobianchetti.1984@gmail.com"
+              className="text-[var(--color)] underline transition-all duration-500 hover:font-bold"
+            >
+              alessandrobianchetti.1984@gmail.com
+            </a>
           </div>
         </div>
         <div className="flex items-center">
           <div className="flex-shrink-0 w-10 h-10 bg-indigo-100 rounded-full flex items-center justify-center">
-            {/* Location icon placeholder */}
-            <MdLocationOn className="text-[var(--color)] text-2xl" />
+            <MapPin className="text-[var(--color)] w-6 h-6" />
           </div>
           <div className="ml-4">
-            <p className=" font-medium text-[var(--color)]">Indirizzo</p>
-            <p className=" text-[var(--color)] underline hover:font-bold">
-              <a
-                href="https://www.google.com/maps/place/kamehouse+studio/@41.8910821,12.5221564,17z/data=!3m1!4b1!4m6!3m5!1s0x132f61024cde5375:0x1a57d377453a50e1!8m2!3d41.8910821!4d12.5221564!16s%2Fg%2F11wmr8vhtj?entry=ttu&g_ep=EgoyMDI0MTAyNy4wIKXMDSoASAFQAw%3D%3D"
-                target="_blank"
-              >
-                Via Prenestina 60,b
-              </a>
-            </p>
+            <p className="font-medium text-[var(--color)]">Indirizzo</p>
+            <a
+              href="https://www.google.com/maps/place/kamehouse+studio/@41.8910821,12.5221564,17z/data=!3m1!4b1!4m6!3m5!1s0x132f61024cde5375:0x1a57d377453a50e1!8m2!3d41.8910821!4d12.5221564!16s%2Fg%2F11wmr8vhtj?entry=ttu&g_ep=EgoyMDI0MTAyNy4wIKXMDSoASAFQAw%3D%3D"
+              target="_blank"
+              className="text-[var(--color)] underline transition-all duration-500 hover:font-bold"
+            >
+              Via Prenestina 60,b
+            </a>
           </div>
         </div>
       </div>
