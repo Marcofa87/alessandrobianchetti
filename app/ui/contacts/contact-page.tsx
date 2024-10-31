ffafc77d27c9e4651f5d3528b5c338519d5f4f84
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-10-31 20:02:00 +0100

    implemented hover effect to footer icons

--- app/ui/contacts/contact-page.tsx
+++ app/ui/contacts/contact-page.tsx
@@ -7,7 +7,7 @@ export default function ContactPage() {
       <div className="space-y-4">
         <div className="flex items-center">
           <div className="flex-shrink-0 w-10 h-10 bg-indigo-100 rounded-full flex items-center justify-center">
-            <Phone className="text-[var(--color)] w-6 h-6" />
+            <Phone className="text-[var(--color)] w-6 h-6 transition-all duration-300 hover:font-bold" />
           </div>
           <div className="ml-4">
             <p className="font-medium text-[var(--color)]">
@@ -16,7 +16,7 @@ export default function ContactPage() {
             <a
               href="https://wa.me/393394955810"
               target="_blank"
-              className="text-[var(--color)] underline transition-all duration-500 hover:font-bold"
+              className="text-[var(--color)] underline transition-all duration-300 hover:font-bold"
             >
               +39 3394955810
             </a>
@@ -24,13 +24,13 @@ export default function ContactPage() {
         </div>
         <div className="flex items-center">
           <div className="flex-shrink-0 w-10 h-10 bg-indigo-100 rounded-full flex items-center justify-center">
-            <Mail className="text-[var(--color)] w-6 h-6" />
+            <Mail className="text-[var(--color)] w-6 h-6 transition-all duration-300 hover:font-bold" />
           </div>
           <div className="ml-4">
             <p className="font-medium text-[var(--color)]">Indirizzo email</p>
             <a
               href="mailto:alessandrobianchetti.1984@gmail.com"
-              className="text-[var(--color)] underline transition-all duration-500 hover:font-bold"
+              className="text-[var(--color)] underline transition-all duration-300 hover:font-bold"
             >
               alessandrobianchetti.1984@gmail.com
             </a>
@@ -38,14 +38,14 @@ export default function ContactPage() {
         </div>
         <div className="flex items-center">
           <div className="flex-shrink-0 w-10 h-10 bg-indigo-100 rounded-full flex items-center justify-center">
-            <MapPin className="text-[var(--color)] w-6 h-6" />
+            <MapPin className="text-[var(--color)] w-6 h-6 transition-all duration-300 hover:font-bold" />
           </div>
           <div className="ml-4">
             <p className="font-medium text-[var(--color)]">Indirizzo</p>
             <a
               href="https://www.google.com/maps/place/kamehouse+studio/@41.8910821,12.5221564,17z/data=!3m1!4b1!4m6!3m5!1s0x132f61024cde5375:0x1a57d377453a50e1!8m2!3d41.8910821!4d12.5221564!16s%2Fg%2F11wmr8vhtj?entry=ttu&g_ep=EgoyMDI0MTAyNy4wIKXMDSoASAFQAw%3D%3D"
               target="_blank"
-              className="text-[var(--color)] underline transition-all duration-500 hover:font-bold"
+              className="text-[var(--color)] underline transition-all duration-300 hover:font-bold"
             >
               Via Prenestina 60,b
             </a>
