44f5938368c2e06fc1739842808062b3e5a0b644
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-10-31 16:07:34 +0100

    styled some parts of the app

--- app/ui/contacts/contact-page.tsx
+++ app/ui/contacts/contact-page.tsx
@@ -15,7 +15,13 @@ export default function ContactPage() {
             <p className=" font-medium text-[var(--color)]">
               Numero di telefono
             </p>
-            <p className=" text-[var(--color)]">+39 123 456 7890</p>
+
+            <p className=" text-[var(--color)] underline hover:font-bold">
+              {" "}
+              <a href="https://wa.me/393394955810" target="_blank">
+                +39 3394955810
+              </a>
+            </p>
           </div>
         </div>
         <div className="flex items-center">
@@ -24,8 +30,12 @@ export default function ContactPage() {
             <MdAlternateEmail className=" text-[var(--color)] text-2xl" />
           </div>
           <div className="ml-4">
-            <p className=" font-medium text-[var(--color)]">Indirizzo email</p>
-            <p className=" text-[var(--color)]">esempio@email.com</p>
+            <p className=" font-medium text-[var(--color)] ">Indirizzo email</p>
+            <p className=" text-[var(--color)] hover:font-bold underline">
+              <a href="mailto:alessandrobianchetti.1984@gmail.com">
+                alessandrobianchetti.1984@gmail.com
+              </a>
+            </p>
           </div>
         </div>
         <div className="flex items-center">
@@ -34,8 +44,15 @@ export default function ContactPage() {
             <MdLocationOn className="text-[var(--color)] text-2xl" />
           </div>
           <div className="ml-4">
-            <p className=" font-medium text-[var(--color)]">Località</p>
-            <p className=" text-[var(--color)]">Roma, Italia</p>
+            <p className=" font-medium text-[var(--color)]">Indirizzo</p>
+            <p className=" text-[var(--color)] underline hover:font-bold">
+              <a
+                href="https://www.google.com/maps/place/kamehouse+studio/@41.8910821,12.5221564,17z/data=!3m1!4b1!4m6!3m5!1s0x132f61024cde5375:0x1a57d377453a50e1!8m2!3d41.8910821!4d12.5221564!16s%2Fg%2F11wmr8vhtj?entry=ttu&g_ep=EgoyMDI0MTAyNy4wIKXMDSoASAFQAw%3D%3D"
+                target="_blank"
+              >
+                Via Prenestina 60,b
+              </a>
+            </p>
           </div>
         </div>
       </div>
