3dc1f50036b9c3e5ffe333f7fdd5660e8bdb5ddf
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-11-11 15:09:42 +0100

    styled contact form and loading skeleton of structure layout

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -4,12 +4,12 @@ import ContactPage from "@/components/ui/contacts/contact-page";
 export default function Page() {
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="flex flex-col items-center lg:flex-row-reverse  lg:justify-between space-y-8 md:space-y-0 md:space-x-8">
-        <div className="w-full max-w-md">
-          <ContactPage />
-        </div>
+      <div className="flex flex-col items-center lg:flex-row  lg:justify-around space-y-8 md:space-y-0 md:space-x-8">
         <div className="w-full max-w-md">
           <ContactForm />
+        </div>
+        <div className="w-full max-w-md">
+          <ContactPage />
         </div>
       </div>
     </div>
--- app/structure/loading.tsx
+++ app/structure/loading.tsx
@@ -1,22 +1,26 @@
 const Loading = () => {
+  const numSkeletons = 3;
+
   return (
-    <div className="w-full max-w-7xl mx-auto p-4">
-      <div className="flex flex-col md:flex-row gap-6">
-        {/* Image skeleton */}
-        <div className="w-full md:w-1/2 h-64 md:h-80 bg-gray-200 animate-pulse rounded-lg"></div>
+    <div className="w-full max-w-7xl mx-auto p-4 ">
+      {Array.from({ length: numSkeletons }).map((_, index) => (
+        <div key={index} className="flex flex-col md:flex-row gap-6 mt-40">
+          {/* Image skeleton */}
+          <div className="w-full md:w-1/2 h-64 md:h-80 bg-gray-200 animate-pulse rounded-lg"></div>
 
-        {/* Content skeleton */}
-        <div className="w-full md:w-1/2 space-y-4">
-          {/* Title skeleton */}
-          <div className="h-8 bg-gray-200 rounded-lg w-3/4 animate-pulse"></div>
+          {/* Content skeleton */}
+          <div className="w-full md:w-1/2 space-y-4">
+            {/* Title skeleton */}
+            <div className="h-8 bg-gray-200 rounded-lg w-3/4 animate-pulse"></div>
 
-          {/* Text lines skeleton */}
-          <div className="space-y-3">
-            <div className="h-4 bg-gray-200 rounded w-full animate-pulse"></div>
-            <div className="h-4 bg-gray-200 rounded w-4/5 animate-pulse"></div>
+            {/* Text lines skeleton */}
+            <div className="space-y-3">
+              <div className="h-4 bg-gray-200 rounded w-full animate-pulse"></div>
+              <div className="h-4 bg-gray-200 rounded w-4/5 animate-pulse"></div>
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
