1ef3b5a050a41f7d30819dd4936ae0686ba63818
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-11-13 18:18:26 +0100

    implemented traduction

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,18 +1,17 @@
 import Link from "next/link";
 import { Dumbbell } from "lucide-react";
+import { useTranslations } from "next-intl";
 
 export default function NotFound() {
+  const t = useTranslations("NotFound");
   return (
     <div className="flex flex-col items-center mt-28">
       <Dumbbell className="my-8" size={128} color="#c1292e" />
-      <h2 className="text-4xl font-bold mb-4">404 - Pagina non trovata</h2>
-      <p className="text-xl mb-8 text-center max-w-md">
-        Oops! Se non trovi la pagina desiderata è perchè anche lei è sicuramente
-        in Palestra!
-      </p>
+      <h2 className="text-4xl font-bold mb-4">{t("error")}</h2>
+      <p className="text-xl mb-8 text-center max-w-md">{t("content")}</p>
       <Link href="/" passHref>
         <button className="px-4 py-2 bg-[var(--tertiary-color)]  rounded hover:bg-red-800 transition font-bold">
-          Torna alla Home
+          {t("home")}
         </button>
       </Link>
     </div>
--- app/ui/success/SuccessPage.tsx
+++ app/ui/success/SuccessPage.tsx
@@ -1,15 +1,17 @@
 import { BicepsFlexed } from "lucide-react";
+import { useTranslations } from "next-intl";
 import Link from "next/link";
 
 export default function SuccessSubmit() {
+  const t = useTranslations("SuccessSubmission");
   return (
     <div className="flex flex-col items-center mt-28 ">
-      <h2 className=" mb-4">Successo!</h2>
-      <p className="text-lg  mb-6">Il tuo form è stato inviato con successo.</p>
+      <h2 className=" mb-4">{t("success")}</h2>
+      <p className="text-lg text-center mb-6">{t("formsubmission")}</p>
       <BicepsFlexed className="my-8" size={128} color="#c1292e" />
       <Link href="/" passHref>
         <button className="px-4 py-2 bg-[var(--tertiary-color)]  rounded hover:bg-red-800 transition font-bold">
-          Torna alla Home
+          {t("home")}
         </button>
       </Link>
     </div>
