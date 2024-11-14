4a3a7449103b4bed58b154332c014d2f604472e9
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-11-14 23:49:44 +0100

    implemented title h1 in the structure page

--- app/ui/structureLayoutPage/StructurePageLayout.tsx
+++ app/ui/structureLayoutPage/StructurePageLayout.tsx
@@ -10,12 +10,12 @@ export default function StructurePageLayout({}: StructurePageLayoutProps) {
   const t = useTranslations("Structure");
   const structureSections = useStructureData();
   return (
-    <div className="container mx-auto py-12">
-      <h1 className="pb-24 sm:text-xs text-center">{t("title")}</h1>
+    <div className="container mx-auto py-12 ">
+      <h1 className="pb-24 text-center">{t("title")}</h1>
       {structureSections.map((section) => (
         <section
           key={section.id}
-          className="p-6 md:flex md:items-center md:even:flex-row-reverse mb-12"
+          className="p-6 md:flex md:items-center md:even:flex-row-reverse mb-12 "
         >
           <div className="md:w-2/3 ">
             <Image
@@ -27,7 +27,7 @@ export default function StructurePageLayout({}: StructurePageLayoutProps) {
             />
           </div>
           <div className="md:w-2/3 ">
-            <h2 className="text-center">{section.title}</h2>
+            <h2 className="text-center py-8">{section.title}</h2>
             <p className="px-6 mt-8 mx-auto max-w-96 text-center ">
               {section.content}
             </p>
