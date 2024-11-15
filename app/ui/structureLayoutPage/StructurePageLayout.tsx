4b7cacddd93b151b8a59cfd30e1a97416b0689f6
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-11-15 15:42:26 +0100

    solved layout bug in structure

--- app/ui/structureLayoutPage/StructurePageLayout.tsx
+++ app/ui/structureLayoutPage/StructurePageLayout.tsx
@@ -13,15 +13,20 @@ export default function StructurePageLayout({}: StructurePageLayoutProps) {
   return (
     <div className="container mx-auto py-12">
       <h1 className="pb-24 text-center">{t("title")}</h1>
-      {structureSections.map((section) => (
-        <>
-          <h2 className="text-center py-8">{section.title}</h2>
-          <section
-            key={section.id}
-            className="p-6 md:flex md:items-center md:even:flex-row-reverse mb-12"
+      {structureSections.map((section, index) => (
+        <section key={section.id} className="p-6 mb-12">
+          {/* Titolo centrale */}
+          <h2 className="text-center pb-16">{section.title}</h2>
+
+          {/* Contenitore immagine e contenuto con flex */}
+          <div
+            className={`md:flex md:items-center ${
+              index % 2 === 1 ? "md:flex-row-reverse" : ""
+            }`}
           >
-            <div className="md:w-2/3 flex justify-center items-center">
-              <div className="relative w-[400px] h-[250px]">
+            {/* Immagine */}
+            <div className="md:w-2/3 flex justify-center items-center ">
+              <div className="relative w-[350px] h-[250px]">
                 <Image
                   src={section.image.src}
                   alt={section.image.alt}
@@ -31,13 +36,13 @@ export default function StructurePageLayout({}: StructurePageLayoutProps) {
                 />
               </div>
             </div>
+
+            {/* Contenuto */}
             <div className="md:w-2/3">
-              <p className="px-6 mt-8 mx-auto max-w-96 text-center">
-                {section.content}
-              </p>
+              <p className="px-8 mt-8 mx-autotext-center">{section.content}</p>
             </div>
-          </section>
-        </>
+          </div>
+        </section>
       ))}
     </div>
   );
