b1743d1f26d2936c0e8575324f688b7117d7e55e
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-11-15 13:25:11 +0100

    implemented color change when icons are hovered

--- app/ui/footer/Footer.tsx
+++ app/ui/footer/Footer.tsx
@@ -5,22 +5,22 @@ export default function Footer() {
   return (
     <footer className="flex justify-end w-3/4 h-20 md:max-w-3xl mx-auto mt-12">
       <a href="https://www.instagram.com/alexroma84/" target="_blank">
-        <FiInstagram className="mr-4 text-2xl transform transition-transform duration-200 hover:-translate-y-1" />
+        <FiInstagram className="mr-4 text-2xl transform transition-transform duration-200 hover:-translate-y-1 hover:text-[var(--tertiary-color)]" />
       </a>
       <a
         href="https://www.linkedin.com/in/alessandro-bianchetti-53562a88/"
         target="_blank"
       >
-        <FiLinkedin className="text-2xl mr-3 transform transition-transform duration-200 hover:-translate-y-1" />
+        <FiLinkedin className="text-2xl mr-3 transform transition-transform duration-200 hover:-translate-y-1 hover:text-[var(--tertiary-color)]" />
       </a>
       <a
         href="https://www.facebook.com/alessandro.bianchetti.39/"
         target="_blank"
       >
-        <FiFacebook className="text-2xl mr-3 transform transition-transform duration-200 hover:-translate-y-1" />
+        <FiFacebook className="text-2xl mr-3 transform transition-transform duration-200 hover:-translate-y-1 hover:text-[var(--tertiary-color)]" />
       </a>
       <a href="https://wa.me/393394955810" target="_blank">
-        <FaWhatsapp className="text-2xl transform transition-transform duration-200 hover:-translate-y-1" />
+        <FaWhatsapp className="text-2xl transform transition-transform duration-200 hover:-translate-y-1 hover:text-[var(--tertiary-color)]" />
       </a>
     </footer>
   );
