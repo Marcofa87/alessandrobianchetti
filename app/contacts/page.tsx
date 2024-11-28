dda60e25b7249ca588acae578c5205e4b7817172
Marco <88092779+Marcofa87@users.noreply.github.com>
2024-11-28 14:43:55 +0100

    solving bugs

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,5 +1,5 @@
 // app/contact/page.tsx
-/* import ContactForm from "@/components/ui/contacts/contact-form"; */
+import ContactForm from "@/components/ui/contacts/contact-form";
 import ContactPage from "@/components/ui/contacts/contact-page";
 import { Metadata } from "next";
 
@@ -33,9 +33,9 @@ export default function Page() {
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="flex flex-col items-center lg:flex-row lg:justify-around mx-auto space-y-8 md:space-y-0 md:space-x-8">
-        {/* <div className="w-full max-w-md">
+        <div className="w-full max-w-md">
           <ContactForm />
-        </div> */}
+        </div>
         <div className="w-full max-w-md">
           <ContactPage />
         </div>
